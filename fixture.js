6a52fb141474ad6dbdde2804740724e33f62ee6b
Durran Jordan <durran@gmail.com>
2021-02-23 22:53:39 +0100

    Adding fixture

---
+++ fixture.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: "Microbursts",
+        intervals: [
+            {
+                name: "Warm Up",
+                over: 0.8,
+                under: 0.5,
+                duration: 600,
+                cadence: 85,
+                repetitions: 1,
+                type: "rampUp"
+            },
+            {
+                name: "Over/Under",
+                over: 3.0,
+                timeOver: 8,
+                cadenceOver: 110,
+                under: 0.8,
+                timeUnder: 52,
+                cadenceUnder: 85,
+                repetitions: 10,
+                type: "overUnder"
+            },
+            {
+                name: "Rest",
+                rest: 0.6,
+                duration: 300,
+                cadence: 85,
+                repetitions: 1,
+                type: "rest"
+            },
+            {
+                name: "Over/Under",
+                over: 3.0,
+                timeOver: 8,
+                cadenceOver: 110,
+                under: 0.8,
+                timeUnder: 52,
+                cadenceUnder: 85,
+                repetitions: 10,
+                type: "overUnder"
+            },
+            {
+                name: "Rest",
+                rest: 0.6,
+                duration: 300,
+                cadence: 85,
+                repetitions: 1,
+                type: "rest"
+            },
+            {
+                name: "Over/Under",
+                over: 3.0,
+                timeOver: 8,
+                cadenceOver: 110,
+                under: 0.8,
+                timeUnder: 52,
+                cadenceUnder: 85,
+                repetitions: 10,
+                type: "overUnder"
+            },
+            {
+                name: "Cool Down",
+                over: 0.8,
+                under: 0.5,
+                duration: 600,
+                cadence: 85,
+                repetitions: 1,
+                type: "rampDown"
+            }
+        ]
+    }
+]
+
